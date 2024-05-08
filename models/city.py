567cbc2dc03cb4aee1d07a8233480264a73a1a76
bg-miles <piuschukwuebuka40@gmail.com>
2024-05-08 18:07:07 +0100

    db

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("state.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    state = relationship("State", back_populates="cities")
--- models/state.py
+++ models/state.py
@@ -2,6 +2,9 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -9,3 +12,15 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade= "all, delete-orphan", back_populates="states")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
