e849067c7cadf1eaa56723a8a5a674fa6881fccf
bg-miles <piuschukwuebuka40@gmail.com>
2024-06-10 17:00:54 +0100

    dep

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["52.204.68.47", "54.167.174.214"]
 
 
 def do_pack():
